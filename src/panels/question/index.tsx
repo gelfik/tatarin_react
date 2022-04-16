c164d26f50ce719ca747e9986883f08f266f1086
ghostara666 <enigmahardy14@gmail.com>
2022-04-16 18:27:59 +0300

    add start page

--- src/panels/question/index.tsx
+++ src/panels/question/index.tsx
@@ -1,19 +1,60 @@
-import { ContentCard, PanelHeader } from "@vkontakte/vkui";
+import {
+  Button,
+  Div,
+  FixedLayout,
+  FormItem,
+  FormLayout,
+  Group,
+  Header,
+  Input,
+  PanelHeader,
+  Radio,
+  RadioGroup,
+} from "@vkontakte/vkui";
 import { observer } from "mobx-react";
 import { Fragment } from "react";
 import "../style.css";
 
 const Question = observer(() => {
-
   return (
     <Fragment>
       <PanelHeader>Вопрос</PanelHeader>
-      <ContentCard
-        onClick={() => {}}
-        src="https://images.unsplash.com/photo-1603988492906-4fb0fb251cf8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80"
-        header="Эчпочмак или Элеш или Чакчак или Кыстыбый или Губадия?"
-        text="Тут варианты ответа"
-      />
+      <Div className={"bg_main_image"}>
+        <Group
+          className={"fill_radioButton"}
+          header={<Header>Текст вопроса</Header>}
+        >
+          <FormLayout>
+            <FormItem top="Выберите вариант ответа">
+              <RadioGroup>
+                <Radio name="fit" value="classic">
+                  Classic
+                </Radio>
+                <Radio name="fit" value="regular">
+                  Regular
+                </Radio>
+                <Radio name="fit" value="slim">
+                  Slim
+                </Radio>
+              </RadioGroup>
+
+              <Input type="text" defaultValue="Петров" />
+            </FormItem>
+          </FormLayout>
+        </Group>
+      </Div>
+      <FixedLayout vertical={"bottom"}>
+        <Div>
+          <Button
+            className={"button_width"}
+            size={"l"}
+            mode={"secondary"}
+            onClick={() => {}}
+          >
+            Дальше!
+          </Button>
+        </Div>
+      </FixedLayout>
     </Fragment>
   );
 });
